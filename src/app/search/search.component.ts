c407ba57fa8b11a9e2fceb93b4928dadf698314c
meskill <meskill@mail.ru>
2016-09-19 17:41:08 +0300

    update search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { NestopiaService } from '../services/nestopia.service';
+
 @Component({
 	selector: 'app-search',
 	templateUrl: './search.component.html',
@@ -9,7 +11,12 @@ export class SearchComponent {
 	title = 'PropertyCross';
 	instruction = `Use the form below to search for houses to buy. You can search by place-name, postcode, or click 'My location',
 	to search in your current location!`;
+	search = '';
 
-	constructor() {
+	constructor(private nestopiaService: NestopiaService) {
+	}
+
+	onSearch() {
+		this.nestopiaService.search(this.search).subscribe(res => console.log(res));
 	}
 }
--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -1,16 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { SearchComponent } from './search.component';
+import { NestopiaService } from '../services/nestopia.service';
 
 @NgModule({
 	declarations: [
 		SearchComponent
 	],
 	imports: [
-		CommonModule
+		CommonModule,
+		FormsModule
 	],
-	providers: [],
+	providers: [NestopiaService],
 	bootstrap: [SearchComponent],
 	exports: [
 		SearchComponent
